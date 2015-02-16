f0e15e1a4e297ba253b3a8f6ba5f7fd93a9f8e15
Christopher Gervais <chris@ergonlogic.com>
2015-02-16 10:23:47 -0500

    Support ANSIBLE_DEBUG env var to see all command output.

--- install-ansible.sh
+++ install-ansible.sh
@@ -25,13 +25,22 @@ which ansible >/dev/null 2>&1
 if [ $? -eq 1 ]; then
 
   echo "Installing Ansible build dependencies."
-  apt-get -qq --force-yes update > /dev/null 2>&1
-  apt-get -qq --force-yes install git python-setuptools >/dev/null 2>&1
+  if [ -z $ANSIBLE_DEBUG ]; then
+    apt-get -qq --force-yes update > /dev/null 2>&1
+    apt-get -qq --force-yes install git python-setuptools >/dev/null 2>&1
+  else
+    apt-get --force-yes update
+    apt-get --force-yes install git python-setuptools
+  fi
 
   ansible_dir=/usr/local/lib/ansible/
   if [ ! -d $ansible_dir ]; then
     echo "Cloning Ansible."
-    git clone --quiet --recursive --depth=1 git://github.com/ansible/ansible.git $ansible_dir >/dev/null 2>&1
+    if [ -z $ANSIBLE_DEBUG ]; then
+      git clone --quiet --recursive --depth=1 git://github.com/ansible/ansible.git $ansible_dir >/dev/null 2>&1
+    else
+      git clone --recursive --depth=1 git://github.com/ansible/ansible.git $ansible_dir
+    fi
   fi
 
   if [ -z $checkout ] && [ ! -z $ANSIBLE_CHECKOUT ]; then
@@ -42,11 +51,19 @@ if [ $? -eq 1 ]; then
   if [ $checkout ]; then
     echo "Checking out '$checkout'."
     cd $ansible_dir
-    git checkout $checkout --quiet
+    if [ -z $ANSIBLE_DEBUG ]; then
+      git checkout $checkout --quiet
+    else
+      git checkout $checkout
+    fi
   fi
 
   echo "Running setups tasks for Ansible."
   cd $ansible_dir
-  python ./setup.py install >/dev/null 2>&1
+  if [ -z $ANSIBLE_DEBUG ]; then
+    python ./setup.py install >/dev/null 2>&1
+  else
+    python ./setup.py install
+  fi
 
 fi

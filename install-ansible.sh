ff87805f6fe5dec15b032aa42b1899db90efa4f9
Jez.Cordonnier <jez.cordonnier@fridaymediagroup.com>
2016-06-07 10:16:29 +0100

    Added dependencies to both debug and non debug added both force yes and assume yes

--- install-ansible.sh
+++ install-ansible.sh
@@ -33,11 +33,11 @@ if [ $? -eq 1 ]; then
 
   echo "Installing Ansible build dependencies."
   if [ -z $ANSIBLE_DEBUG ]; then
-    apt-get -qq --assume-yes update > /dev/null 2>&1
-    apt-get -qq --assume-yes install git python-setuptools python-dev > /dev/null 2>&1
+    apt-get -qq --force-yes --assume-yes update > /dev/null 2>&1
+    apt-get -qq --force-yes --assume-yes install git python-cryptography python-setuptools python-dev python-paramiko python-yaml python-jinja2 python-httplib2 python-passlib python-six python-ecdsa > /dev/null 2>&1
   else
     apt-get --assume-yes update
-    apt-get --assume-yes install git python-setuptools python-dev python-paramiko python-yaml python-jinja2 python-httplib2 python-passlib python-six python-ecdsa
+    apt-get --assume-yes install git python-cryptography python-setuptools python-dev python-paramiko python-yaml python-jinja2 python-httplib2 python-passlib python-six python-ecdsa
   fi
 
   if [ -z $branch ] && [ ! -z $ANSIBLE_BRANCH ]; then
